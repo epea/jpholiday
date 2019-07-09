512654acefaa05750dce8a9b27d60d209e548059
Yoshitake Kitamura <kitamura@epea.co.jp>
2019-07-09 22:03:58 +0900

    動作環境のみ

---
+++ jpholiday/src/main/java/jp/co/epea/jpholiday/App.java
@@ -0,0 +1,13 @@
+package jp.co.epea.jpholiday;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	var a = "Foo";
+    }
+}
---
+++ jpholiday/src/test/java/jp/co/epea/jpholiday/AppTest.java
@@ -0,0 +1,14 @@
+package jp.co.epea.jpholiday;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AppTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
